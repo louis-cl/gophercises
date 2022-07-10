b8ed6ac7de8f019577439ef62bc58f9e98582e7a
Louis Clergue <louisclergue@gmail.com>
2022-07-10 14:05:41 +0200

    First iteration of quiz game

---
+++ 1-quiz-game/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func createProblemList(data [][]string) []problem {
+	var problems []problem
+	for _, line := range data {
+		problems = append(problems, problem{line[0], line[1]})
+	}
+	return problems
+}
+
+func play(problems []problem) {
+	scanner := bufio.NewScanner(os.Stdin)
+	correctAnswers := 0
+	for i, prob := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, prob.question)
+		scanner.Scan()
+		answer := scanner.Text()
+		if answer == prob.answer {
+			correctAnswers++
+		}
+	}
+	fmt.Printf("You scored %d out of %d", correctAnswers, len(problems))
+}
+
+func main() {
+	var (
+		csvFile = flag.String("csv", "problems.csv", "a csv file in the format of question,answer")
+		// timeLimit = flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	)
+	flag.Parse()
+	// read file
+	f, err := os.Open(*csvFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	// parse csv
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convert data to problems
+	problems := createProblemList(data)
+	play(problems)
+}
