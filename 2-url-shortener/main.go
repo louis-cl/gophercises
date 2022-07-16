e405387f91d03020351860a77fe041d9663fc32e
Louis Clergue <louis.clergue@king.com>
2022-07-16 15:01:56 +0200

    Accept optional yaml map file

--- 2-url-shortener/main.go
+++ 2-url-shortener/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 	"urlshort/handler"
 )
 
 func main() {
+	var mapFile = flag.String("routes", "", "path to yaml file with routes")
+	flag.Parse()
+
+	yamlConfig := readOrDefault(*mapFile)
+
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -16,20 +23,29 @@ func main() {
 	}
 	mapHandler := handler.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := handler.YAMLHandler([]byte(yaml), mapHandler)
+	// Build the YAMLHandler using the mapHandler as the fallback
+	yamlHandler, err := handler.YAMLHandler(yamlConfig, mapHandler)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func readOrDefault(filePath string) []byte {
+	if len(filePath) == 0 {
+		return []byte(`
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`)
+	}
+	bytes, err := os.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
 }
 
 func defaultMux() *http.ServeMux {
