1b72ea548f8ee766d3d0796965908a11ca0ce479
Louis Clergue <louis.clergue@king.com>
2022-07-20 21:36:36 +0200

    output xml to stdout

--- 5-sitemap/main.go
+++ 5-sitemap/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
-	"fmt"
+	"io"
+	"os"
 	"sitemap/scraper"
 )
 
@@ -10,7 +12,36 @@ func main() {
 	var domain = flag.String("domain", "example.com", "domain to build a sitemap for")
 	flag.Parse()
 	res := scraper.AllLinks(*domain)
-	for _, link := range res {
-		fmt.Println(link)
+	writeXml(os.Stdout, res)
+}
+
+func writeXml(out io.Writer, urls []string) {
+	out.Write([]byte(xml.Header))
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", "  ")
+	var out_urls []*xml_url
+	for _, url := range urls {
+		out_urls = append(out_urls, &xml_url{Loc: url})
 	}
+	enc.Encode(xml_urlset{Xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9", Urls: out_urls})
+}
+
+// <?xml version="1.0" encoding="UTF-8"?>
+// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+//   <url>
+//     <loc>http://www.example.com/</loc>
+//   </url>
+//   <url>
+//     <loc>http://www.example.com/dogs</loc>
+//   </url>
+// </urlset>
+
+type xml_urlset struct {
+	XMLName xml.Name   `xml:"urlset"`
+	Xmlns   string     `xml:"xmlns,attr"`
+	Urls    []*xml_url `xml:"url"`
+}
+
+type xml_url struct {
+	Loc string `xml:"loc"`
 }
