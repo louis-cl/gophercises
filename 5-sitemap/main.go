72a51c5a30ec875d368b6fa03f4f3b841ea4bb0a
Louis Clergue <louis.clergue@king.com>
2022-07-20 20:02:27 +0200

    Recursive domain exploration

--- 5-sitemap/main.go
+++ 5-sitemap/main.go
@@ -1,30 +1,73 @@
 package main
 
 import (
+	"container/list"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"path"
 	"sitemap/parser"
+	"strings"
 )
 
 func main() {
 	var domain = flag.String("domain", "example.com", "domain to build a sitemap for")
 	flag.Parse()
 
-	links, err := linksIn(fmt.Sprintf("http://%s", *domain))
+	startingUrl := fmt.Sprintf("http://%s", *domain)
+
+	toProcess := list.New()
+	processed := make(map[string]bool)
+
+	toProcess.PushBack(startingUrl)
+	processed[startingUrl] = false
+
+	for toProcess.Len() > 0 {
+		item := toProcess.Front().Value.(string)
+		links, err := linksIn(item)
+		if err != nil {
+			log.Println("couldn't get links for", item, err)
+		}
+
+		toProcess.Remove(toProcess.Front())
+		processed[item] = true
+		for _, link := range links {
+			log.Println("found link", link)
+			if _, in := processed[link]; !in && isLinkInDomain(link, *domain) {
+				processed[link] = false
+				toProcess.PushBack(link)
+			}
+		}
+	}
+
+	for link, _ := range processed {
+		fmt.Printf("%v", link)
+	}
+}
+
+func isLinkInDomain(link string, domain string) bool {
+	u, err := url.Parse(link)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%v", links)
+	parts := strings.Split(u.Hostname(), ".")
+	if len(parts) < 2 {
+		return u.Hostname() == domain
+	}
+	link_domain := parts[len(parts)-2] + "." + parts[len(parts)-1]
+	return link_domain == domain
 }
 
 // linksIn returns the links present in the page at given url
 // returned links are complete absolute http links
-func linksIn(url string) ([]string, error) {
-	resp, err := http.Get(url)
+func linksIn(web string) ([]string, error) {
+	log.Println("scanning ", web)
+	u, err := url.Parse(web)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.Get(web)
 	if err != nil {
 		return nil, err
 	}
@@ -35,9 +78,9 @@ func linksIn(url string) ([]string, error) {
 	}
 	var result []string
 	for _, link := range links {
-		new_url, err := mergeUrl(link.Href, url)
+		new_url, err := mergeUrl(link.Href, u)
 		if err != nil {
-			log.Println("Cannot merge url ", new_url, err)
+			log.Println("Cannot merge url", new_url, err)
 		} else {
 			result = append(result, new_url)
 		}
@@ -45,15 +88,13 @@ func linksIn(url string) ([]string, error) {
 	return result, nil
 }
 
-func mergeUrl(child string, parent string) (string, error) {
+func mergeUrl(child string, parent *url.URL) (string, error) {
 	u, err := url.Parse(child)
 	if err != nil {
 		return "", err
 	}
-	if u.Scheme == "" { // relative link
-		return path.Join(parent, u.Path), nil
-	} else if u.Scheme == "http" || u.Scheme == "https" { // absolute link
-		return child, nil
+	if u.Scheme == "" || u.Scheme == "http" || u.Scheme == "https" {
+		return parent.ResolveReference(u).String(), nil
 	} else {
 		return "", fmt.Errorf("unknown scheme %s", u.Scheme)
 	}
